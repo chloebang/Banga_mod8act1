b3751df464ca5ba15aaa3584a4f7f4fd152a1a32
chloebang <126551420+chloebang@users.noreply.github.com>
2023-02-28 15:06:32 +0800

    Add files via upload

---
+++ skeleton.js
@@ -0,0 +1,17 @@
+let person = prompt("Hello! Who are you?");
+document.getElementById("fname").innerHTML = person;
+
+person = prompt("No, no, your ONLINE name");
+document.getElementById("username").innerHTML = person;
+
+person = prompt("So what's your gender?");
+document.getElementById("gender").innerHTML = person;
+
+person = prompt("Now how would you describe yourself?");
+document.getElementById("desc").innerHTML = person;
+
+person = prompt("What was the year you were born?");
+document.getElementById("year").innerHTML = person;
+
+person = prompt("So that makes you...?");
+document.getElementById("age").innerHTML = person;
